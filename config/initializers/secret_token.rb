5cb94fbd90109d90fd0053b4342bb6bcafc0f190
orenbus <orenbus@gmail.com>
2011-10-15 18:11:15 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RonPaulCountry::Application.config.secret_token = '6422ff19537882b67be6ba29ebc8c8a0f8acf81c0002420212769a97da01396f8e3052d7a68074bdc8768c17f0863ec4033cb06ed309eaf567f2c9b06e0f99e4'
